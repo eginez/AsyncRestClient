6ca38e737952465d44c686f9b884c2f4b8e0b2b9
Esteban <eginez@gmail.com>
2015-12-04 22:09:00 -0800

    Changing deps to reactivex org. Making the library a category

--- src/main/groovy/org/eginez/AsyncRestClient.groovy
+++ src/main/groovy/org/eginez/AsyncRestClient.groovy
@@ -8,42 +8,13 @@ import rx.schedulers.Schedulers
 import static rx.util.async.Async.*
 
 class AsyncRestClient {
-    @Delegate protected RESTClient restClient
-
-
-    /**
-     * Constructor.
-     * @see groovyx.net.http.HTTPBuilder#HTTPBuilder()
-     */
-    public AsyncRestClient() {
-        restClient = new RESTClient()
-    }
-
-    /**
-     * See {@link groovyx.net.http.HTTPBuilder#HTTPBuilder(Object)}
-     * @param defaultURI default request URI (String, URI, URL or {@link groovyx.net.http.URIBuilder})
-     * @throws URISyntaxException
-     */
-    public AsyncRestClient( Object defaultURI ) throws URISyntaxException {
-        restClient = new RESTClient(defaultURI)
-    }
-
-    /**
-     * See {@link groovyx.net.http.HTTPBuilder#HTTPBuilder(Object, Object)}
-     * @param defaultURI default request URI (String, URI, URL or {@link groovyx.net.http.URIBuilder})
-     * @param defaultContentType default content-type (String or {@link groovyx.net.http.ContentType})
-     * @throws URISyntaxException
-     */
-    public AsyncRestClient( Object defaultURI, Object defaultContentType ) throws URISyntaxException {
-        restClient = new RESTClient(defaultURI, defaultContentType)
-    }
 
     /**
      * Convience method to performed a GET using observables
      * @param args named parameters - see
      *  {@link HTTPBuilder.RequestConfigDelegate#setPropertiesFromMap(Map)}
      */
-    public Observable<Object> getAsync(Map<String,?> args, Scheduler scheduler = Schedulers.io()){
+    public static Observable<Object> getAsync(RESTClient restClient, Map<String,?> args, Scheduler scheduler = Schedulers.io()){
         return start({ restClient.get(args) }, scheduler)
     }
 
@@ -52,7 +23,7 @@ class AsyncRestClient {
      * @param args named parameters - see
      *  {@link HTTPBuilder.RequestConfigDelegate#setPropertiesFromMap(Map)}
      */
-    public Observable<Object> postAsync( Map<String,?> args, Scheduler scheduler = Schedulers.io() ) {
+    public static Observable<Object> postAsync( RESTClient restClient, Map<String,?> args, Scheduler scheduler = Schedulers.io() ) {
         return start({ restClient.post(args) }, scheduler)
     }
 
@@ -61,7 +32,7 @@ class AsyncRestClient {
      * @param args named parameters - see
      *  {@link HTTPBuilder.RequestConfigDelegate#setPropertiesFromMap(Map)}
      */
-    public Observable<Object> putAsync( Map<String,?> args, Scheduler scheduler = Schedulers.io() ) {
+    public static Observable<Object> putAsync(RESTClient restClient, Map<String,?> args, Scheduler scheduler = Schedulers.io() ) {
         return start({ restClient.put(args) }, scheduler)
     }
 
@@ -70,8 +41,7 @@ class AsyncRestClient {
      * @param args named parameters - see
      *  {@link HTTPBuilder.RequestConfigDelegate#setPropertiesFromMap(Map)}
      */
-    public Observable<Object> deleteAsync( Map<String,?> args, Scheduler scheduler = Schedulers.io()){
+    public static Observable<Object> deleteAsync(RESTClient restClient, Map<String,?> args, Scheduler scheduler = Schedulers.io()){
         return start({ restClient.delete(args) }, scheduler)
     }
-
 }
--- src/test/groovy/org/eginez/AsyncRestClientTest.groovy
+++ src/test/groovy/org/eginez/AsyncRestClientTest.groovy
@@ -1,8 +1,9 @@
 package org.eginez
 
-import groovy.mock.interceptor.MockFor
 import groovyx.net.http.RESTClient
 import org.junit.Test
+import org.mockito.Matchers
+import org.mockito.Mockito
 
 import java.util.concurrent.CountDownLatch
 
@@ -10,60 +11,44 @@ class AsyncRestClientTest {
 
 
     @Test
-    public void testSync() {
-        def httpClient = new MockFor(RESTClient)
-        httpClient.demand.get { 'get' }
-        httpClient.use {
-            def s = new AsyncRestClient('http://properUrl')
-            def res = s.getAsync(path: '/')
-                    .toBlocking()
-                    .single()
-            assert res == 'get'
-        }
-    }
-
-    @Test
     public void testGetAsync() {
-        def httpClient = new MockFor(RESTClient)
-        httpClient.demand.get { 'get' }
+        def httpClient = Mockito.mock(RESTClient.class)
+        Mockito.when(httpClient.get(Matchers.any())).thenReturn('get')
         testAsync(httpClient, 'getAsync')
     }
 
     @Test
     public void testPostAsync() {
-        def httpClient = new MockFor(RESTClient)
-        httpClient.demand.post { 'post' }
+        def httpClient = Mockito.mock(RESTClient.class)
+        Mockito.when(httpClient.get(Matchers.any())).thenReturn('get')
         testAsync(httpClient, 'postAsync')
     }
 
     @Test
     public void testPutAsync() {
-        def httpClient = new MockFor(RESTClient)
-        httpClient.demand.put { 'put' }
+        def httpClient = Mockito.mock(RESTClient.class)
+        Mockito.when(httpClient.get(Matchers.any())).thenReturn('get')
         testAsync(httpClient, 'putAsync')
     }
 
     @Test
     public void testDeleteAsync() {
-        def httpClient = new MockFor(RESTClient)
-        httpClient.demand.delete { 'delete' }
+        def httpClient = Mockito.mock(RESTClient.class)
+        Mockito.when(httpClient.get(Matchers.any())).thenReturn('get')
         testAsync(httpClient, 'deleteAsync')
     }
 
 
-    private void testAsync(MockFor mock, String method) {
-        mock.use {
-            def s = new AsyncRestClient('https://someurl.com')
-            def cd = new CountDownLatch(1)
-            def success = false
-            s."$method"(path: '/').subscribe { res ->
-                res == method
-                success = true
-                cd.countDown()
-            }
-            cd.await()
-            assert success
+    private void testAsync(RESTClient mock, String method) {
+        def cd = new CountDownLatch(1)
+        def success = false
+        AsyncRestClient."$method"(mock, [path: '/']).subscribe { res ->
+            res == method
+            success = true
+            cd.countDown()
         }
+        cd.await()
+        assert success
     }
 
 }
